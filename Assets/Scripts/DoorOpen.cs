68cec2280962480875a8d3f48829a7d0ea5741f4
MONGAN\ongan <mehmetongan86@gmail.com>
2024-04-22 21:49:43 +0300

    First Commit

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public float theDistance;
+    public GameObject actionKey;
+    public GameObject actionText;
+    public GameObject hinge;
+    public AudioSource doorSound;
+ 
+    void Update()
+    {
+        theDistance = PlayerRay.distanceFromTarget;
+
+      
+    }
+    private void OnMouseOver()
+    {
+        if (theDistance <= 2)
+        {
+            actionKey.SetActive(true);
+            actionText.SetActive(true);
+        }
+
+        else
+        {
+            
+        }
+        if (Input.GetButton("Action"))
+        {
+            if (theDistance <= 2)
+            {
+                this.gameObject.GetComponent<BoxCollider>().enabled = false;
+                actionKey.SetActive(false);
+                actionText.SetActive(false);
+                hinge.GetComponent<Animation>().Play("MyDoorAnim");
+                doorSound.Play();
+            }
+        }
+            
+    }
+    private void OnMouseExit()
+    {
+        
+        actionKey.SetActive(false);
+        actionText.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerRay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRay : MonoBehaviour
+{
+    public static float distanceFromTarget; //alacagımız nesnelere olan uzaklık olduğu içinjm bunu static yaptık böylece her yerden koda erişebiliriz
+    public float toTarget;//Bu da gördugumuz nesnelere olan uzaklık kapı duvar veya herhangi bir nesne
+    // Start is called before the first frame update
+ 
+    void Update()
+    {
+       RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100.0f));
+        toTarget = hit.distance;
+        distanceFromTarget = toTarget;
+
+    }
+}
